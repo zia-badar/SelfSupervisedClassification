5dbe856c24d39a4ab94d3d7cb4a6cdd69ed73cff
zia <zia.badar@campus.tu-berlin.de>
2022-08-17 20:28:04 +0200

    self-supervised classification now shuffles labelled data

--- classifier.py
+++ classifier.py
@@ -14,6 +14,8 @@ class ContrastiveWeightedKNN():
         self.k = k
 
     def w_knn(self, test_x):
+        self.train_x = self.train_x[torch.randperm(self.train_x.shape[0])]
+        self.train_y = self.train_y[torch.randperm(self.train_y.shape[0])]
         train_x = self.train_x[:(int)(self.train_subset_ratio * len(self.train_x))]
         train_y = self.train_y[:(int)(self.train_subset_ratio * len(self.train_y))]
 
